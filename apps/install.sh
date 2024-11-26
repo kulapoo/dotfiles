f3010857021548bd48e04e07b04121647c6b4d0d
kulapoo <kulaskulapoo@gmail.com>
2024-11-26 22:09:05 +0800

    update

--- apps/install.sh
+++ apps/install.sh
@@ -136,6 +136,22 @@ install_yakuake() {
     fi
 }
 
+install_sublime_merge() {
+    if ! command -v smerge &> /dev/null; then
+        log_info "Installing Sublime Merge..."
+
+        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+        echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+        sudo apt-get update
+        sudo apt-get install sublime-merge
+    else
+        log_info "Sublime merge is already installed"
+    fi
+}
+
+
+install_sublime_merge
+
 # Konsole
 install_konsole() {
     if ! command -v konsole &> /dev/null; then
--- test-install.sh
+++ test-install.sh
@@ -8,17 +8,3 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DOTFILES_DIR}/utils/logging.sh"
 
 
-install_sublime() {
-    if ! command -v subl &> /dev/null; then
-        log_info "Installing Sublime Text..."
-        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-        echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-        sudo apt-get update
-        sudo apt-get install sublime-text
-    else
-        log_info "Sublime Text is already installed"
-    fi
-}
-
-
-install_sublime
